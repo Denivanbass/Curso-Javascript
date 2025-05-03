e2fb7897b5e7d35dfc837663462e62ce49fbdb8f
estellinha07 <estellavilarouca.g@gmail.com>
2025-05-03 12:20:48 -0300

    api

---
+++ axios-fakestore/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { ProductList } from './components/ProductList'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Catálogo Fakestore</h1>
+      <ProductList />
+    </div>
+  )
+}
+
+export default App
